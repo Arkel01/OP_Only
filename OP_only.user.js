f0602bdd1f7b676cd0c54386db6e1a49faee4f5e
Arkel01 <sullivan.71@hotmail.fr>
2022-07-17 20:54:41 +0200

    Patch fonctionnel et visuel
    - Changement du visuel du bouton.
    - Correction d'un bug qui n'affichait pas tous les messages si le script était appelé depuis une autre page que la première.
    - Légers ajustements dans l'écriture du script.

--- OP_only.user.js
+++ OP_only.user.js
@@ -4,132 +4,123 @@
 // @author       Arkel01
 // @description  Affiche tous les messages de l'auteur d'un topic jeuxvideo.com. Github : https://github.com/Arkel01/OP_Only
 // @icon         http://image.noelshack.com/fichiers/2022/25/3/1655854502-op-only-logo.png
-// @version      1.0.0
+// @version      1.1.0
 // @license      MIT
 // @downloadURL  https://github.com/Arkel01/OP_Only/raw/main/OP_Only.user.js
 // @updateURL    https://github.com/Arkel01/OP_Only/raw/main/OP_Only.user.js
-// @match        http://www.jeuxvideo.com/forums/42-*
 // @match        https://www.jeuxvideo.com/forums/42-*
-// @match        http://www.jeuxvideo.com/forums/1-*
-// @match        https://www.jeuxvideo.com/forums/1-*
-
 // ==/UserScript==
 
-(function() {
-    'use strict';
 
-    function makeRequest (url) {
-        return new Promise(function (resolve, reject) {
-            var xhr = new XMLHttpRequest();
-            xhr.responseType = "document";
-            xhr.open('GET', url);
-            xhr.onload = function () {
-                if (xhr.status >= 200 && xhr.status < 300) {
-                    resolve(xhr.response);
-                } else {
-                    reject({
-                        status: xhr.status,
-                        statusText: xhr.statusText
-                    });
-                }
-            };
-            xhr.onerror = function () {
+function make_request(url) {
+    // Requête xmlhttp qui extraie l'objet document associé à l'URL donnée en argument
+
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = 'document';
+        xhr.open('GET', url);
+        xhr.onload = function () {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.response);
+            } else {
                 reject({
                     status: xhr.status,
                     statusText: xhr.statusText
                 });
-            };
-            xhr.send();
-        });
-    } // Requête xmlhttp qui extraie l'objet document associé à l'URL donnée en argument
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: xhr.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+}
 
-    async function main() { // Fonction exécutée en appuyant sur le bouton
+async function main() { // Fonction exécutée en appuyant sur le bouton
+
+    return new Promise(async resolve => {
+
+        let page_bloc = document.getElementsByClassName('bloc-liste-num-page')[0];
+        let n_pages;
+        (page_bloc.lastChild.textContent == '»') ? n_pages = page_bloc.lastChild.previousSibling.textContent : n_pages = page_bloc.lastChild.textContent; // Quand il y a beaucoup de pages, le nombre de pages est dans l'avant dernier élément, pas le dernier
+
 
         // Suppression des numéros de pages, qui pourraient laisser penser que le script affiche les résultats sur plusieurs pages
-        document.getElementsByClassName("bloc-liste-num-page")[0].remove();
-        document.getElementsByClassName("bloc-liste-num-page")[0].remove();
+        document.getElementsByClassName('bloc-liste-num-page')[0].remove();
+        document.getElementsByClassName('bloc-liste-num-page')[0].remove();
 
-        // Suppression des tous les messages de la page (sauf le 1er, nécessairement de l'auteur)
-        let messages_main_page=document.getElementsByClassName("bloc-message-forum mx-2 mx-lg-0 "); // HTMLCollection de tous les blocs de messages de la page initiale
-        let n_messages_to_del=messages_main_page.length; // On doit stocker le nombre de messages à supprimer car messages_main_page.length var varier à chaque itération, et ne peut donc pas directement être utilisé
-        for(let i=0; i<n_messages_to_del;i++) messages_main_page[0].remove();
+        // Suppression des tous les messages de la page
+        let messages_main_page = document.getElementsByClassName('bloc-message-forum mx-2 mx-lg-0 '); // Array de tous les blocs de messages de la page initiale
+        let messages_main_page_length = messages_main_page.length;
+        for (let element = 0; element < messages_main_page_length - 1; element++) messages_main_page[0].remove();
 
         // Message en haut du topic indiquant la progression du script
-        let progress = document.createElement("div");
-        progress.id="progress";
-        progress.style.position = "relative";
-        progress.style.left = "28%";
-        progress.style.top = "50%";
-        document.getElementsByClassName("conteneur-messages-pagi")[0].insertBefore(progress,document.getElementsByClassName("bloc-pagi-default px-3 px-lg-0")[0]);
+        let progress = document.createElement('div');
+        progress.id = 'progress';
+        progress.style.textAlign = 'center';
+        progress.textContent = 'OP_Only : début de la récupération des messages...';
+        document.getElementsByClassName('conteneur-messages-pagi')[0].insertBefore(progress, document.getElementsByClassName('conteneur-messages-pagi')[0].firstChild);
 
-        progress.innerHTML="Début de la récupération des messages...";
 
-        let last_page=document.getElementsByClassName("xXx pagi-fin-actif icon-next2")[0]; // Vaut undefined s'il n'y a qu'une page
-        let n_pages;
+        let author; // Pseudonyme de l'auteur
 
-        /* L'URL d'un topic s'écrit : "https://www.jeuxvideo.com/forums/XX-XX-XXXXXXXX-?-0-1-0-titre-du-topic.htm". Le "?" correspond à la page du topic
-        On récupère l'URL associée au bouton de la dernière page (i.e l'URL de la dernière page), puis on récupère le "?", qui est donc le nombre de pages. */
-        (last_page==undefined) ? n_pages=1 : n_pages=parseInt(last_page.href.split('-')[3]);
+        for (let current_page_number = 1; current_page_number < parseInt(n_pages) + 1; current_page_number++) { // Itération sur chaque page du topic
 
-        let current_page_document; // Document de la page observée dans la boucle
-        let current_author; // Pseudo du message observé dans la boucle
-        let current_avatar; // Avatar de l'auteur du message observé dans la boucle
-        let current_bloc_avatar; // Bloc de l'avatar du message observé
-        let author; // Pseudo de l'auteur
+            progress.textContent = 'OP Only : récupération des messages en cours : page ' + current_page_number + '/' + parseInt(n_pages); // Modification du message d'entête
 
-        for (let current_page_number = 1; current_page_number < n_pages+1; current_page_number++) { // Itération sur chaque page du topic
-
-            progress.innerHTML="Récupération des messages en cours : page " + current_page_number + "/" + n_pages; // Modification du message d'entête
-
-            // Création de l'URL de la page suivante en modifiant le "?"
-            let url_split=document.URL.split('-');
-            url_split[3]=current_page_number.toString();
+            // Création de l'URL de la page suivante
+            let url_split = document.URL.split('-');
+            url_split[3] = current_page_number.toString();
 
             // Récupération du document de la nouvelle page à observer
-            current_page_document = await makeRequest(url_split.join('-'))
-                .then(function (document) {
-                return document;
-            })
-                .catch(function (err) {
-                console.error('Error in OP Only script (xmlhttpRequest did not resolve).', err.statusText);
-            });
+            let current_page_document = await make_request(url_split.join('-').split('#')[0]);
 
-            let messages=current_page_document.getElementsByClassName("bloc-message-forum mx-2 mx-lg-0 "); // HTMLCollection de tous les blocs de messages de la page observée
+            let messages = current_page_document.getElementsByClassName('bloc-message-forum mx-2 mx-lg-0 '); // Array de tous les blocs de messages de la page observée
 
-            if(current_page_number==1){ // Récupération du pseudo de l'auteur à la page 1
+            if (current_page_number == 1) { // Récupération du pseudonyme de l'auteur à la page 1
                 // Les pseudo sont stockés sous la forme ''\n                            LeDiplodocul\n                        '. Le pseudo commence toujours au 30ème caractère, on l'obtient en l'extrayant de la chaîne de caractères
-                author=messages[0].querySelectorAll("[class$=text-user]")[0].innerHTML.substring(29).split('\n')[0];
+                author = messages[0].querySelectorAll('[class$=text-user]')[0].innerHTML.substring(29).split('\n')[0];
             }
 
-            for(let messages_number=0; messages_number<messages.length; messages_number++){ // Itération sur chaque message de la page
-                current_bloc_avatar=messages[messages_number].querySelectorAll("[class$=text-user]")[0]; // Bloc de l'avatar du message observé
-                if(current_bloc_avatar!=undefined){ // Quand l'auteur du message observé est banni, le bloc avatar associé n'existe pas. Ce n'est donc pas l'auteur
-                    current_author=current_bloc_avatar.innerHTML.substring(29).split('\n')[0]; // On récupère le pseudo de l'auteur du message comme précedemment
-                    if(current_author==author){ // Si le message est observé est bien un message de l'auteur, l'afficher
-                        current_avatar=messages[messages_number].getElementsByClassName("user-avatar-msg js-lazy")[0]; // Les avatars sont déformés lors de la requête http (l'avatar passe de l'attribut 'src' à 'data-src' et doit être réattribué)
-                        current_avatar.setAttribute('src',current_avatar.getAttribute('data-src'));
-                        document.getElementsByClassName("conteneur-messages-pagi")[0].insertBefore(messages[messages_number],document.getElementsByClassName("bloc-message-forum-anchor")[1]);
+            for (let messages_number = 0; messages_number < messages.length; messages_number++) { // Itération sur chaque message de la page
+                let current_bloc_avatar = messages[messages_number].querySelectorAll('[class$=text-user]')[0]; // Bloc de l'avatar du message observé
+                if (current_bloc_avatar != undefined) { // Quand l'auteur du message observé est banni, le bloc avatar associé n'existe pas. Ce n'est donc pas l'auteur
+                    let current_author = current_bloc_avatar.innerHTML.substring(29).split('\n')[0]; // On récupère le pseudonyme de l'auteur du message comme précedemment
+                    if (current_author == author) { // Si le message est observé est bien un message de l'auteur, l'afficher
+                        let current_avatar = messages[messages_number].getElementsByClassName('user-avatar-msg js-lazy')[0]; // Les avatars sont déformés lors de la requête http (l'avatar passe de l'attribut 'src' à 'data-src' et doit être réattribué)
+                        current_avatar.setAttribute('src', current_avatar.getAttribute('data-src'));
+                        document.getElementsByClassName('conteneur-messages-pagi')[0].insertBefore(messages[messages_number], document.getElementsByClassName('bloc-message-forum-anchor')[1]);
                     }
                 }
             }
         }
 
         // Modification du message d'entête et changement de la couleur du bouton pour signifier la fin du script
-        progress.innerHTML="Récupération des messages terminée.";
-        progress.style.left = "32%";
-        btn.style.borderColor="green";
-        this.disabled=true;
-    }
+        progress.textContent = 'OP Only : récupération des messages terminée.';
+        btn.style.borderColor = 'green';
+        this.disabled = true;
 
-    // Création du bouton permettant d'appeler le script, positionné à côté du bouton "Liste des sujets"
-    let btn = document.createElement("button");
-    btn.id="btn";
-    btn.innerHTML = "Messages de l'auteur";
+        resolve();
+    });
+}
 
-    btn.classList.add("btn","btn-actu-new-list-forum");
-    btn.style.setProperty("min-width","8.5rem"); // La largeur du bouton doit être légèrement plus grande que les autres pour pouvoir contenir le texte intérieur
-    document.getElementsByClassName("group-two")[0].appendChild(btn); // Ajout du bouton
+// Création du bouton permettant d'appeler le script, positionné à côté du bouton 'Liste des sujets'
+let btn = document.createElement('button');
+btn.id = 'btn';
+btn.textContent = 'Messages de l\'auteur';
 
-    btn.onclick=main;
+btn.classList.add('btn', 'btn-actu-new-list-forum');
+btn.style.minWidth = '8.5rem'; // La largeur du bouton doit être légèrement plus grande que les autres pour pouvoir contenir le texte intérieur
+document.getElementsByClassName('group-two')[0].appendChild(btn); // Ajout du bouton
 
-})();
+btn.onclick = async function () {
+    this.disabled = 'true';
+    await main();
+    this.style.border = '0.0625rem solid #c28507';
+    this.style.background = '#f0a100';
+    this.style.color = 'white';
+    this.disabled = 'true';
+}
